d35984ad7ff833ca3dd4692e72a1af892ae8281e
ElisVassiltsova <elis3705@gmail.com>
2018-10-17 23:12:09 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <fstream>
 
-
 struct Point{
     float x;
     float y;
@@ -17,10 +16,8 @@ float perimeter(Point Array[4]);
 float area(Point Array[4]);
 QString parseIntoTextFile(Point Array[4]);
 
-
 int main()
 {
-
     Point quadOne[4];
     Point quadTwo[4];
 
@@ -40,7 +37,6 @@ int main()
     quadOne[3].x=-0.0;
     quadOne[3].y=4.0;
 
-
     quadTwo[0].Name='E';
     quadTwo[0].x=-10;
     quadTwo[0].y=-10;
@@ -57,54 +53,33 @@ int main()
     quadTwo[3].x=-10;
     quadTwo[3].y=10;
 
-
     QFile txt("results.txt");
-
     if (txt.open(QFile::WriteOnly | QFile::Truncate)) {
-
         QTextStream writeStream(&txt);
-
         writeStream << parseIntoTextFile(quadOne);
-
         writeStream << parseIntoTextFile(quadTwo);
-
         txt.close();
-
         std:: cout << "Done. Check the \"results.txt\" file in project's bulid directory.";
     }
-
     return 0;
 }
 
-
-
 float distance(Point A, Point B) {
-
     int distance = sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2));
-
     return distance;
 }
 
-
 float perimeter(Point Array[4]) {
-
     float AB = distance(Array[0], Array[1]);
-
     float BC = distance(Array[1], Array[2]);
-
     float CD = distance(Array[2], Array[3]);
-
     float DA = distance(Array[0], Array[3]);
 
-
     float perimeter = AB + BC + CD + DA;
-
     return perimeter;
 }
 
-
 float area(Point Array[4]) {
-
     float AB = distance(Array[0], Array[1]);
     float BC = distance(Array[1], Array[2]);
     float CD = distance(Array[2], Array[3]);
@@ -116,33 +91,24 @@ float area(Point Array[4]) {
 
     float area1 = sqrt(p1 * (p1 - AB) * (p1 - BC) * (p1 - d));
     float area2 = sqrt(p2 * (p2 - CD) * (p2 - DA) * (p2 - d));
-
     float area = area1 + area2;
-
     return area;
-
 }
 
 QString parseIntoTextFile(Point Array[4]) {
-
     QString text, temp;
     Point current;
 
     text = "Quadrilateral with points: ";
-
     for (int i=0; i < 4; i++) {
         current = Array[i];
         temp = "%1[%2;%3] ";
         temp = temp.arg(current.Name).arg(current.x).arg(current.y);
-
         text.append(temp);
     }
-
     temp = "\r\nHas perimeter %1 and area %2\r\n";
     temp = temp.arg(perimeter(Array)).arg(area(Array));
-
     text.append(temp);
 
     return text;
-
 }
